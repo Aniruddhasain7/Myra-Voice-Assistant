e150b6b4a3ef5320d559a1c540adeae7fb9099c3
Aniruddha Sain <aniruddhasain315@gmail.com>
2026-04-08 19:40:55 +0530

    update

--- Myra/src/gemini.js
+++ Myra/src/gemini.js
@@ -2,28 +2,19 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.5-flash-lite",
+});
 
-const generationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 40,
-  maxOutputTokens: 100,
-  responseMimeType: "text/plain",
-};
+export async function run(prompt) {
+  try {
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
 
-async function run(prompt) {
-  
-  const model = genAI.getGenerativeModel({ 
-    model: "gemini-2.5-flash-lite", 
-    generationConfig, 
-  });
-
-  const chatSession = model.startChat({
-    history: [], 
-  });
-
-  const result = await chatSession.sendMessage(prompt);
-  return result.response.text();
+    return response.text();
+  } catch (error) {
+    console.error("Gemini Error:", error);
+    return "Something went wrong";
+  }
 }
-
 export default run;
